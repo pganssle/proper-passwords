1aba0ff776f725d30251dc2abf3c8d85cdbac387
Paul G <p.ganssle@gmail.com>
2014-04-27 16:12:22 -0500

    Settings helper library should work as well as it needs to. Added valid_string_type to input_validation.py
    Signed-off-by: Paul G <p.ganssle@gmail.com>

--- libraries/input_validation.py
+++ libraries/input_validation.py
@@ -5,3 +5,55 @@ Library for easy validation of common inputs.
 @author Paul J. Ganssle
 @since 2014-04
 '''
+import warnings
+
+def valid_string_type(input, throw_error=False, warn=False, repr_on_fail=False,
+						string_types=(str,unicode)):
+	'''
+	Check if the string is valid, with various contingencies defined.
+
+	@param throw_error Throw an error if the string is not valid. [Default: False]
+	@type throw_error bool
+
+	@param warn Raise a warning if the string is not valid [Default: False]
+	@type warn bool
+
+	@param repr_on_fail Return the value printed to a string if it's not an instance of a 
+						 string type. [Default: False] 
+	@type repr_on_fail bool
+
+	@param string_types Valid string types. Default: (str, unicode)
+	@type type or tuple of types
+
+	@return Returns boolean value of whether the string validated or not if repr_on_fail is false.
+	        If repr_on_fail is true, returns repr(input) or input if the string is already valid.
+
+	@throws NotStringTypeError Raised if parameter throw_error is true and string does not validate.
+	@throws NotStringTypeWarning Raised if parameter warn is true and string does not validate.
+	'''
+
+	if not isinstance(input, string_types):
+		if throw_error:
+			raise NotStringTypeError('Type is '+type(input).__name__+' with value '+repr(input))
+
+		if warn:
+			warnings.warn('Invalid string type', NotStringTypeWarning)
+
+		return repr(input) if repr_on_fail else False
+	else:
+		return input if repr_on_fail else True
+
+
+
+# Exceptions
+class NotStringTypeError(TypeError):
+	'''
+	Raised if the string is not a valid string type.
+	'''
+	pass
+
+class NotStringTypeWarning(Warning):
+	'''
+	Raised if the string is not a valid string type.
+	'''
+	pass
--- libraries/settings_helper.py
+++ libraries/settings_helper.py
@@ -7,26 +7,24 @@ Provides a clean API for reading and writing settings.
 '''
 
 import os, json
+from shutil import copyfile
+from input_validation import valid_string_type
 
 class SettingsHelper:
 	'''
 	Static class with useful variables.
 	'''
+	# Where to find relevant things
 	base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
 	pref_dir = os.path.join(base_dir, 'preferences')
 	source_dir = os.path.join(base_dir, 'sources')
-	settings_loc = os.path.join(base_dir, 'settings.json')
+	settings_loc = os.path.join(pref_dir, 'settings.json')
+	default_settings_loc = os.path.join(base_dir, 'default_settings.json')
 	settings_version = 0.1
 
 	# Settings keys
 	version_key = 'version'
 	markov_source_loc_key = 'markov_source_loc'
-
-class SettingsWriter:
-	'''
-	Helper class for altering the settings file.
-	'''
-	pass
 
 class SettingsReader:
 	'''
@@ -34,12 +32,13 @@ class SettingsReader:
 	'''
 
 	# Settings file location, defined relative to the location of this library.
+	_read_file_loc = ''
 	_settings_dict = None
 	_settings_replacements = {'$base_dir' : SettingsHelper.base_dir,
 							  '$pref_dir' : SettingsHelper.pref_dir,
 							  '$source_dir' : SettingsHelper.source_dir}
 
-	def __init__(self):
+	def __init__(self, file_loc=SettingsHelper.settings_loc):
 		'''
 		Constructor for the settings reader, reads the settings into a dict. The settings file
 		should be a single dict, encoded with JSON.
@@ -49,14 +48,16 @@ class SettingsReader:
 									  containing a settings dictionary.
 		'''
 
-		if not os.path.exists(__settings_loc):
-			raise NoSettingsFileError('Settings file not found at '+SettingsHelper.__settings_loc)
+		if not os.path.exists(file_loc):
+			raise NoSettingsFileError('Settings file not found at '+file_loc)
 
-		with open(SettingsHelper.__settings_loc, 'r') as settings_file_object:
+		self._read_file_loc = file_loc
+
+		with open(file_loc, 'r') as settings_file_object:
 			self._settings_dict = json.load(settings_file_object)
 
 		# Check the current version (check for validity)
-		if SettingsHelper._version_key not in self._settings_dict.keys():
+		if SettingsHelper.version_key not in self._settings_dict.keys():
 			raise BadSettingsFileError('Settings file version not found')
 		
 		self._file_version = self._settings_dict[SettingsHelper.version_key]
@@ -70,7 +71,7 @@ class SettingsReader:
 
 		@return Returns the value of the setting.
 
-		@throws KeyError Thrown if an invalid setting is requested.
+		@throws InvalidSettingError Thrown if an invalid setting is requested.
 		'''
 
 		return self._parse_setting(self.getRawValue(key))
@@ -86,8 +87,8 @@ class SettingsReader:
 
 		@throws KeyError Thrown if an invalid setting is requested.
 		'''
-		if not hasSetting(key):
-			raise KeyError('Setting "'+key+'" not found in the settings file.')
+		if not self.hasSetting(key):
+			raise InvalidSettingError('Setting "'+key+'" not found in the settings file.')
 
 		return self._settings_dict[key]
 
@@ -115,13 +116,104 @@ class SettingsReader:
 				returned.
 		'''
 
-		if not isinstance(setting_value, str):
+		if not valid_string_type(setting_value, throw_error=False):
 			return setting_value
 
 		for key in self._settings_replacements.keys():
 			setting_value = setting_value.replace(key, self._settings_replacements[key])
 
 		return setting_value
+
+class SettingsWriter(SettingsReader):
+	'''
+	Helper class for altering the settings file.
+	'''
+	_save_file_loc = ''
+
+	def __init__(self, load_file=SettingsHelper.settings_loc, 
+			           save_file_loc=SettingsHelper.settings_loc):
+		'''
+		Constructor for the settings writer.
+
+		@param load_file The the settings file upon which to base the settings. If this is a dict, 
+						 it is assumed to be an already-loaded settings dictionary. 
+		@type load_file str or dict
+
+		@param save_file_loc Where to save the changes to the file.
+		@type save_file_loc str
+
+		@throws NoSettingsFileError Thrown when the old settings file does not exist.
+		@throws BadSettingsFileError Thrown when the old settings file is not valid.
+		'''
+		if isinstance(load_file, dict):
+			self._settings_dict = load_file
+		else:
+			super(SettingsWriter, self).__init__(file_loc=load_file)
+
+		self._save_file_loc=save_file_loc
+
+
+	def setValue(self, key, setting_value):
+		'''
+		Alter a value in the settings dictionary.
+		'''
+		if not self.hasSetting(key):
+			raise InvalidSettingError('Could not find setting "'+key+'" in settings dictionary.')
+
+		# JSON cannot handle None values.
+		if setting_value is None:
+			raise InvalidSettingValue('Cannot store NoneType in settings file.')
+
+		self._settings_dict[key] = setting_value
+
+	def writeValues(self):
+		'''
+		Write the changes to file.
+		'''
+
+		# Make the directory if it does not exist
+		if not os.path.exists(os.path.dirname(self._save_file_loc)):
+			os.makedirs(os.path.dirname(self._save_file_loc))
+
+		with open(self._save_file_loc, 'w+') as settings_file:
+			json.dump(self._settings_dict, fp=settings_file, 
+				      indent=4, 						# Pretty print
+				      separators=(',', ': '))			# No trailing whitespace
+
+def restore_default_settings():
+	'''
+	Restores the default settings.
+
+	@throws IOError Thrown if the default settings file is not readable or the settings file is not
+	                writable.
+	'''
+	if not os.path.exists(SettingsHelper.default_settings_loc):
+		generate_default_settings_file()
+
+	copyfile(SettingsHelper.default_settings_loc, SettingsHelper.settings_loc)
+
+
+def generate_default_settings_file(file_loc=SettingsHelper.default_settings_loc):
+	'''
+	Generates a default settings file.
+
+	@param file_loc Location to generate the default settings file in.
+	@type file_loc str
+	'''
+	# Make the directory if it does not exist
+	if not os.path.exists(os.path.dirname(file_loc)):
+		os.makedirs(os.path.dirname(self._save_file_loc))
+
+	# The default settings dictionary:
+	settings_dict = dict()
+	settings_dict[SettingsHelper.version_key] = SettingsHelper.settings_version
+	settings_dict[SettingsHelper.markov_source_loc_key] = '$source_dir/markov_sources'
+
+	# Save the file
+	SW = SettingsWriter(load_file=settings_dict, save_file_loc=file_loc)
+	SW.writeValues();
+
+
 
 # Exceptions
 class NoSettingsFileError(Exception):
@@ -134,5 +226,17 @@ class BadSettingsFileError(Exception):
 	'''
 	Raised when a settings file exists, but does not contain a JSON object encoding a settings
 	dictionary.
+	'''
+	pass
+
+class InvalidSettingError(KeyError):
+	'''
+	Raised when an invalid setting is requested for reading or writing.
+	'''
+	pass
+
+class InvalidSettingValue(ValueError):
+	'''
+	Raised when an invalid value is passed to a setting.
 	'''
 	pass
